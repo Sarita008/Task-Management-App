9d918f97cbbe8a83464035d0f78ec31ccbfaa664
Sarita008 <saritasharma8201@gmail.com.com>
2026-07-10 10:19:10 +0530

    backend deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://task-management-app-3i9b.vercel.app/api',
+    baseURL: 'https://task-management-app-bygj.onrender.com/api',
     // baseURL: 'http://localhost:5000/api',
 });
 
