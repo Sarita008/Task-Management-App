6a6991ca634f1fca93cbb2cfec6fd4d9731c9585
Sarita008 <saritasharma8201@gmail.com.com>
2026-07-10 09:02:09 +0530

    backend connected

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: '<PROD_URL>/api',
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://task-management-app-3i9b.vercel.app/api',
+    // baseURL: 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
